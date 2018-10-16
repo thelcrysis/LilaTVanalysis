b99cbbaf14bb0cea1a8eaf19e2e466c3e2c734b7
thelcrysis <thelcrysis@gmail.com>
2018-10-17 01:05:53 +0200

    Saveing data to txt file in format "date->no of watchers"

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+from time import gmtime, strftime
 url = 'https://lichess.org/tv'
-
+file = open("fo.txt","a")
 browser = webdriver.Firefox()
 browser.get(url)
-for i in range(10):
-    time.sleep(3)
-    print(browser.find_element_by_class_name("number").text)
+currentNumberofWatcher = 0
+while(1):
+    time.sleep(2)
+    if (currentNumberofWatcher != browser.find_element_by_class_name("number").text):
+        currentNumberofWatcher = browser.find_element_by_class_name("number").text
+        if (currentNumberofWatcher != None):
+            output = (strftime("%d %m %Y %H:%M:%S", gmtime()) + "-> " + currentNumberofWatcher)
+            file.write(output + '\n')
+            print(output)
+    else:
+        print "-||-"
+fo.close()
 browser.close()
